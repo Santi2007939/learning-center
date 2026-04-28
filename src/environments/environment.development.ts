9ef2d82001fe4129ca9e8bd8b88ea9d5c6dd13f9
santi2007939 <fabrizioprogramador939@gmail.com>
2026-04-28 11:43:33 -0500

    feat(environemnt): add developer environment configuration and file replacements.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderCategoriesEndpointPath: '/categories',
+  platformProviderCoursesEndpointPath: '/courses',
+  platformProviderSignInEndpointPaht: '/authentication/sign-in',
+  platformProviderSignUpEndpointPath: '/authentication/sign-up',
+  logoProviderApiBaseUrl: 'https://img.logo.dev',
+  }
+;
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+    platformProviderCategoriesEndpointPath: '/categories',
+    platformProviderCoursesEndpointPath: '/courses',
+    platformProviderSignInEndpointPaht: '/authentication/sign-in',
+    platformProviderSignUpEndpointPath: '/authentication/sign-up',
+    logoProviderApiBaseUrl: 'https://img.logo.dev',
+  }
+;
